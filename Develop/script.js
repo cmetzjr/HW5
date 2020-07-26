f69a59db8bca8b3d95bb87449f8e266669100c87
Charles Metzger <cmetzjr@yahoo.com>
2020-07-25 23:30:43 -0400

    before adding save to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,40 +1,67 @@
 //array of times
-var times = ["8AM", "9AM", "10AM", "11AM", "12AM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM"];
+// var times = ["8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM", "9PM", "10PM", "11PM"];
+
+var times = [
+    { display: "8AM", value: 08 },
+    { display: "9AM", value: 09 },
+    { display: "10AM", value: 10 },
+    { display: "11AM", value: 11 },
+    { display: "12PM", value: 12 },
+    { display: "1PM", value: 13 },
+    { display: "2PM", value: 14 },
+    { display: "3PM", value: 15 },
+    { display: "4PM", value: 16 },
+    { display: "5PM", value: 17 },
+    { display: "6PM", value: 18 },
+    { display: "7PM", value: 19 },
+    { display: "8PM", value: 20 },
+    { display: "9PM", value: 21 },
+    { display: "10PM", value: 22 },
+    { display: "11PM", value: 23 }
+]
+
 
 //compare the hour in each time block to the current hour, then set a class accordingly
 function classSetter() {
     //get the current hour (in format matching the array)
-    var currentHour = moment().format("hA");
+    var currentHour = moment().format("H");
     //get the value of each time block
-    var hourBlock = timeBlock.attr("data-value");//not sure about syntax
-
-    if (currentHour > hourBlock) {
-        timeBlock.addClass("past");
-    } else if (currentHour < hourBlock) {
-        timeBlock.addclass("future");
+    var hourBlock = textArea.attr("data-value");
+    //log both values to make sure the numbers make sense
+    console.log(hourBlock, currentHour);
+    //compare the hourBlock to the current time and style appropriately --> isn't working
+    if (hourBlock > currentHour) {
+        textArea.addClass("future");
+    } else if (hourBlock < currentHour) {
+        textArea.addClass("past");
     } else {
-        timeBlock.addClass("present");
-    };
-};
+        textArea.addClass("present");
+    }
+}
 
 //create a timeBlock for each hour in the array
 for (i = 0; i < times.length; i++) {
     //create <span> with class of "hour" pulling the hour from the array
-    const hourSpan = $("<span>").addClass("hour").text(times[i]);
-    //create <textarea> with some classes and attributes
-    const textArea = $("<textarea>").addClass("time-block description").attr("name", "todo-text").attr("id", "todo-text").attr("cols", "50").attr("rows", "4");
+    var hourSpan = $("<span>").addClass("hour").text(times[i].display);
+    //create <textarea> with classes, attributes and a data-value equal to the hour
+    var textArea = $("<textarea>").addClass("time-block description").attr("name", "todo-text").attr("id", "todo-text").attr("cols", "50").attr("rows", "4").attr("data-value", times[i].value);
     //create a button with a class and the save icon inside
-    const saveBtn = $("<button>").addClass("saveBtn").html('<i class="far fa-save fa-2x"></i>');
-    //create a timeBlock div with classes and a data-value equal to the hour
-    const timeBlock = $("<div>").addClass("row row-col time-block").attr("data-value", times[i]);
+    var saveBtn = $("<button>").addClass("saveBtn").html('<i class="far fa-save fa-2x"></i>');
+    //create a timeBlock div with classes 
+    var timeBlock = $("<div>").addClass("row row-col time-block");
     //append the hour, textarea, and save button to the timeBlock row
     timeBlock.append(hourSpan).append(textArea).append(saveBtn);
     //append the row to the container
     $(".container").append(timeBlock);
+    //run the function every 30 sec to compare check each time slot against the current time and apply the correct style
+    window.setInterval(classSetter(), 30000);
+}
 
-    // classSetter();//not sure about usage
-
-};
 
 
-//when you type in the box, it saves the text to local storage
+//when saveBtn is clicked, text is saved to local storage
+
+saveBtn.click(function () {
+
+
+}); 
